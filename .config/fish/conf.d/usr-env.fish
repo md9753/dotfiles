d97b94df09aa95fd5c7077925a15cb802a33959c
matt <junkmailtwopointoh@gmail.com>
2025-07-04 13:33:56 -0500

    remove unused envvar

--- .config/fish/conf.d/usr-env.fish
+++ .config/fish/conf.d/usr-env.fish
@@ -2,9 +2,6 @@ set -gx EDITOR hx
 
 set -gx STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship/starship.toml"
 
-# for a python script -- not sure if this is the best place for it
-set -gx MONITOR_HEIGHT 850
-
 # fzf stuff
 set FZF_DEFAULT_OPTS '--bind "ctrl-y:execute-silent(string trim {} | wl-copy -n)"'
 set FZF_DEFAULT_COMMAND 'fd -u'
